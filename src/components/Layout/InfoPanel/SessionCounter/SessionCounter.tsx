c34c2de01125d9ca1735e9ef61043b177afc32b4
andrey <andreases_mobimoz@ukr.net>
2023-04-05 22:47:54 +0300

    added feature sessionsCounter

--- src/components/Layout/InfoPanel/SessionCounter/SessionCounter.tsx
+++ src/components/Layout/InfoPanel/SessionCounter/SessionCounter.tsx
@@ -1,5 +1,28 @@
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
+
+const SERVER_URL = "http://localhost:8080";
+
 export const SessionCounter = () => {
-  const sessionCount = 100;
+  const [sessionCount, setSessionCount] = useState(0);
+
+  useEffect(() => {
+    if (typeof window === undefined) {
+      return;
+    }
+
+    const socket = io(SERVER_URL);
+
+    socket.on("subscribers", handleConnections);
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  const handleConnections = (data: number) => {
+    setSessionCount(data);
+  };
 
   return (
     <div>

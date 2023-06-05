e898a86b34378dd7f64a2e6a8076f0c57da025a6
andrey <andreases_mobimoz@ukr.net>
2023-06-05 23:04:08 +0300

    Added card component

--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -8,10 +8,17 @@ interface ProductListProps {
 
 export const ProductList: FC<ProductListProps> = ({ products }) => {
   return (
-    <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-      {products.map(({ title, id, photo, prices }) => (
+    <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+      {products.map(({ title, id, photo, isAvailable, price, shortDescription }) => (
         <li key={id}>
-          <ProductCard id={id} title={title} photo={photo} prices={prices} />
+          <ProductCard
+            id={id}
+            title={title}
+            photo={photo}
+            price={price}
+            shortDescription={shortDescription}
+            isAvailable={isAvailable}
+          />
         </li>
       ))}
     </ul>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import { IProductResponse } from "@/interfaces";
 import { Home } from "@/screens/Home";
-import { productService } from "@/services/product/product.service";
 import { detectErrorMessage } from "@/utils/detect-error-message";
 import { errorAlert } from "@/utils/error-alert";
 import { FC } from "react";
@@ -22,7 +21,68 @@ export default HomePage;
 
 export const getServerSideProps = async () => {
   try {
-    const res = await productService.getAllProducts();
+    const res: IProductResponse = {
+      count: 0,
+      currentPage: 0,
+      products: [
+        {
+          id: 1,
+          isAvailable: true,
+          photo: "/test-product.jpeg",
+          price: 100,
+          shortDescription:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. In, nisi et inventore necessitatibus officiis hic, commodi suscipit soluta nostrum perferendis laboriosam qui quia ipsum dolores iste voluptas facere possimus aut.",
+          title: "Laptop Pro",
+        },
+
+        {
+          id: 2,
+          isAvailable: false,
+          photo: "/test-product.jpeg",
+          price: 100,
+          shortDescription:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. In, nisi et inventore necessitatibus officiis hic, commodi suscipit soluta nostrum perferendis laboriosam qui quia ipsum dolores iste voluptas facere possimus aut.",
+          title: "Laptop Pro",
+        },
+        {
+          id: 3,
+          isAvailable: true,
+          photo: "/test-product.jpeg",
+          price: 100,
+          shortDescription:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. In, nisi et inventore necessitatibus officiis hic, commodi suscipit soluta nostrum perferendis laboriosam qui quia ipsum dolores iste voluptas facere possimus aut.",
+          title: "Laptop Pro",
+        },
+        {
+          id: 4,
+          isAvailable: true,
+          photo: "/test-product.jpeg",
+          price: 100,
+          shortDescription:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. In, nisi et inventore necessitatibus officiis hic, commodi suscipit soluta nostrum perferendis laboriosam qui quia ipsum dolores iste voluptas facere possimus aut.",
+          title: "Laptop Pro",
+        },
+        {
+          id: 5,
+          isAvailable: true,
+          photo: "/test-product.jpeg",
+          price: 100,
+          shortDescription:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. In, nisi et inventore necessitatibus officiis hic, commodi suscipit soluta nostrum perferendis laboriosam qui quia ipsum dolores iste voluptas facere possimus aut.",
+          title: "Laptop Pro",
+        },
+        {
+          id: 6,
+          isAvailable: true,
+          photo: "/test-product.jpeg",
+          price: 100,
+          shortDescription:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. In, nisi et inventore necessitatibus officiis hic, commodi suscipit soluta nostrum perferendis laboriosam qui quia ipsum dolores iste voluptas facere possimus aut.",
+          title: "Laptop Pro",
+        },
+      ],
+      totalPages: 0,
+    };
 
     return {
       props: { ...res },
